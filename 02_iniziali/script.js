c5fa111346af86a1e9dd038e13f2cd962f9af00b
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-19 09:38:56 +0200

    add part1 ex 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function letterList() {
+    const iniziali = [];
+    for (let i = 0; i < names.length; i++) {
+
+        iniziali.push(names[i].charAt(0));
+    }
+    return iniziali;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const Elenco_lettere = letterList(names)
 
+console.log(Elenco_lettere)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
